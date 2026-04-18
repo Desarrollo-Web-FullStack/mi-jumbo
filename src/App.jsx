8ccb85f08e30b6c300c4ed72174ba7df95f21f74
Fabian Serna <fabianserna2013@gmail.com>
2026-04-17 19:21:08 -0500

    react-router instalado

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+
 function App() {
   return (
-    <h1 className="text-red-500">Hola Mundo</h1>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   )
 }
 
---
+++ src/pages/About.jsx
@@ -0,0 +1,5 @@
+function About() {
+  return <h1 className="text-3xl font-bold text-blue-500 mt-4">Acerca de nosotros</h1>;
+}
+
+export default About;
