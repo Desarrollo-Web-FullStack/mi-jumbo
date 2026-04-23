17ea0591051fdc9601dee06ce7862eb78f70518d
Fabian Serna <fabianserna@gmail.com>
2026-04-23 15:59:28 +0200

    AuthContext & ProtectedRoute agregado

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Login from './pages/Login';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/sobre-nosotros" element={<About />} />
+      <Route path="/" element={<ProtectedRoute element={<Home />} />} />
+      <Route path="/sobre-nosotros" element={<ProtectedRoute element={<About />} />} />
       <Route path="/iniciar-sesion" element={<Login />} />
     </Routes>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
+import './index.css'
+import "./back/firebase.js";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import "./back/firebase.js";
-import './index.css'
+import { AuthProvider } from './context/AuthContext.jsx'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
 )
