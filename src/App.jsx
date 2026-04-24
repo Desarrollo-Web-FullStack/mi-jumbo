71dafd73462b1af4883a80a92b17889b6d6687d2
Fabian Serna <fabianserna@gmail.com>
2026-04-25 01:28:05 +0200

    Componente PublicRoute agregado

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Login from './pages/Login';
 import ProtectedRoute from './utils/routes/ProtectedRoute';
+import PublicRoute from './utils/routes/PublicRoute';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<ProtectedRoute element={<Home />} />} />
       <Route path="/sobre-nosotros" element={<ProtectedRoute element={<About />} />} />
-      <Route path="/iniciar-sesion" element={<Login />} />
+      <Route path="/iniciar-sesion" element={<PublicRoute element={<Login />} />} />
     </Routes>
   )
 }
